5d9daf81afdabc95c02556e6c3aa9711134389b9
AlexKozh <kojemiako-sascha@yandex.ru>
2022-08-31 23:41:51 +0700

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "анастасия")
+{
+    Console.WriteLine("Ура, это же найденешь ");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
